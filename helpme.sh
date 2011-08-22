0b14a1b0469a77712f76607dde0c84d34b290190
ngsw <ngsw@ngsw.jp>
2011-08-23 03:35:19 +0900

    helpme.sh

---
+++ helpme.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+while :
+do
+      clear
+      echo '                 \033[32m\ﾀｽｹﾃｰ/\033[m
+               \033[31m\(´・ω・`)/\033[m' && sleep 3 ; clear
+      echo '\nﾀｽｹﾃｰ > \033[31m\(´・ω・\`)\033[m' && sleep 3 ; clear
+      echo '\n      \033[31m(/´・ω・`)/\033[m < ﾀｽｹﾃｰ' && sleep 3 ; clear
+      echo '\n      \033[31m(´・ω・`)\033[m' && sleep 2 ; clear
+      echo '\n      \033[32m(´；ω；｀)ぶわっ\033[m' && sleep 3 ; clear
+      echo '\n      \033[31m(´・ω・`)\033[m' && sleep 1 ; clear
+      echo '\n      \033[32m(´；ω；｀)ぶわっ\033[m' && sleep 2 ; clear
+      echo '\n      \033[31m(´・ω・`)\033[m' && sleep 1 ; clear
+done
+
