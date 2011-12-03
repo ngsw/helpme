c00282178d3f7a4ed9c4885ec1e7dbbf635e0b3f
ngsw <ngsw@ngsw.jp>
2011-12-03 17:19:46 +0900

    ESCcode を 変数にした   以下を参考   http://www.m-bsys.com/linux/echo-color-1

--- helpme.sh
+++ helpme.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
+#http://www.m-bsys.com/linux/echo-color-2
+#ESCcode
+ESC="\033" ; ECHO="echo"
+#ESC="\e"  ; ECHO="echo -e"
 while :
 do
       clear
-      echo '                 \033[32m\ﾀｽｹﾃｰ/\033[m
-               \033[31m\(´・ω・`)/\033[m' && sleep 3 ; clear
-      echo '\nﾀｽｹﾃｰ > \033[31m\(´・ω・\`)\033[m' && sleep 3 ; clear
-      echo '\n      \033[31m(/´・ω・`)/\033[m < ﾀｽｹﾃｰ' && sleep 3 ; clear
-      echo '\n      \033[31m(´・ω・`)\033[m' && sleep 2 ; clear
-      echo '\n      \033[32m(´；ω；｀)ぶわっ\033[m' && sleep 3 ; clear
-      echo '\n      \033[31m(´・ω・`)\033[m' && sleep 1 ; clear
-      echo '\n      \033[32m(´；ω；｀)ぶわっ\033[m' && sleep 2 ; clear
-      echo '\n      \033[31m(´・ω・`)\033[m' && sleep 1 ; clear
+      ${ECHO} "                 ${ESC}[32m\ﾀｽｹﾃｰ/${ESC}[m
+               ${ESC}[31m\(´・ω・\`)/${ESC}[m" && sleep 3 ; clear
+      ${ECHO} "\nﾀｽｹﾃｰ > ${ESC}[31m\(´・ω・\\\`)${ESC}[m" && sleep 3 ; clear
+      ${ECHO} "\n      ${ESC}[31m(/´・ω・\`)/${ESC}[m < ﾀｽｹﾃｰ" && sleep 3 ; clear
+      ${ECHO} "\n      ${ESC}[31m(´・ω・\`)${ESC}[m" && sleep 2 ; clear
+      ${ECHO} "\n      ${ESC}[32m(´；ω；｀)ぶわっ${ESC}[m" && sleep 3 ; clear
+      ${ECHO} "\n      ${ESC}[31m(´・ω・\`)${ESC}[m" && sleep 1 ; clear
+      ${ECHO} "\n      ${ESC}[32m(´；ω；｀)ぶわっ${ESC}[m" && sleep 2 ; clear
+      ${ECHO} "\n      ${ESC}[31m(´・ω・\`)${ESC}[m" && sleep 1 ; clear
 done
 
